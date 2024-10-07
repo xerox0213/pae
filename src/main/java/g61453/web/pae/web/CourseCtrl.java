2dfba26f87d612b07eb3ed8426d5871e489c4046
Nasreddine Boutouil <nasreddine021@gmail.com>
2024-10-07 18:16:46 +0200

    feat: ajout d'une route qui gère le détail d'un cours

--- src/main/java/g61453/web/pae/web/CourseCtrl.java
+++ src/main/java/g61453/web/pae/web/CourseCtrl.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
 
 @Controller
 public class CourseCtrl {
@@ -17,5 +20,12 @@ public class CourseCtrl {
         Iterable<Course> courses = paeService.getCourses();
         model.addAttribute("courses", courses);
         return "courses";
+    }
+
+    @GetMapping("/courses/{acronym}")
+    public String courseDetail(@PathVariable String acronym, Model model) {
+        Optional<Course> optCourse = paeService.getCourse(acronym);
+        model.addAttribute("course", optCourse.orElse(null));
+        return "course-detail";
     }
 }
