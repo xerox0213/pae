49d45a162b63e75b50dbcfd7912257a1526ee1f4
Nasreddine Boutouil <nasreddine021@gmail.com>
2024-10-07 13:33:20 +0200

    feat: initialisation du projet

---
+++ src/test/java/g61453/web/pae/PaeApplicationTests.java
@@ -0,0 +1,13 @@
+package g61453.web.pae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
