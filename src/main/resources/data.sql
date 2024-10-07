27e6b1900edb5f7aa2d32813ee51444e10c59046
Nasreddine Boutouil <nasreddine021@gmail.com>
2024-10-07 17:02:19 +0200

    feat: création script peuplement base de données
    Peuplement de la table course

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into course (acronym, title, credits)
+values ('1DEV1A', 'Développement 1', 6),
+       ('1EXP1A', 'Exploitation des systèmes 1', 5),
+       ('1ALG1A', 'Algorithmique 1', 4),
+       ('1ANG1A', 'Anglais 1', 2);
