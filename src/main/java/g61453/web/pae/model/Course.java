336194469ad986d7ca3b6b6d2841f8f0d09d2a3f
Nasreddine Boutouil <nasreddine021@gmail.com>
2024-10-07 14:10:08 +0200

    feat: création de l'entité course

---
+++ src/main/java/g61453/web/pae/model/Course.java
@@ -0,0 +1,18 @@
+package g61453.web.pae.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Course {
+    @Id
+    private String acronym;
+    private String title;
+    private int credits;
+}
