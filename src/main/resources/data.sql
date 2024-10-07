b570853384b01de9de32b205a6ae0aaff5994843
Nasreddine Boutouil <nasreddine021@gmail.com>
2024-10-07 17:17:11 +0200

    feat: peuplement de la table students

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,9 @@ values ('1DEV1A', 'Développement 1', 6),
        ('1EXP1A', 'Exploitation des systèmes 1', 5),
        ('1ALG1A', 'Algorithmique 1', 4),
        ('1ANG1A', 'Anglais 1', 2);
+
+insert into student (name, gender, section)
+values ('Damon', 'M', 'APPLICATION'),
+       ('Stefan', 'M', 'NETWORK'),
+       ('Elena', 'F', 'INDUSTRIAL'),
+       ('Nasreddine', 'M', 'APPLICATION');
