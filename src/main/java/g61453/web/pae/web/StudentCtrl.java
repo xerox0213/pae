69c9a2f1acc2adca270cf95ba2adc240df11593d
Nasreddine Boutouil <nasreddine021@gmail.com>
2024-10-09 17:43:59 +0200

    feat: création de la fonctionnalité d'ajout d'un nouvel étudiant

--- src/main/java/g61453/web/pae/web/StudentCtrl.java
+++ src/main/java/g61453/web/pae/web/StudentCtrl.java
@@ -1,5 +1,7 @@
 package g61453.web.pae.web;
 
+import g61453.web.pae.model.Gender;
+import g61453.web.pae.model.Section;
 import g61453.web.pae.model.Student;
 import g61453.web.pae.service.PaeService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Optional;
 
@@ -19,6 +22,9 @@ public class StudentCtrl {
     public String index(Model model) {
         Iterable<Student> students = paeService.getStudents();
         model.addAttribute("students", students);
+        model.addAttribute("student", new Student());
+        model.addAttribute("genders", Gender.values());
+        model.addAttribute("sections", Section.values());
         return "students";
     }
 
@@ -27,5 +33,11 @@ public class StudentCtrl {
         Optional<Student> optStudent = paeService.getStudent(number);
         model.addAttribute("student", optStudent.orElse(null));
         return "student-detail";
+    }
+
+    @PostMapping("/students/add-student")
+    public String addStudent(Student student) {
+        paeService.addStudent(student);
+        return "redirect:/students";
     }
 }
