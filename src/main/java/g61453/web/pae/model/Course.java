d5cde2a0113d4a7f0276da5c60d752a04a1d4073
Nasreddine Boutouil <nasreddine021@gmail.com>
2024-10-10 15:36:07 +0200

    feat: gestion de la validation des données de l'entité course

--- src/main/java/g61453/web/pae/model/Course.java
+++ src/main/java/g61453/web/pae/model/Course.java
@@ -3,6 +3,9 @@ package g61453.web.pae.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,8 +18,12 @@ import java.util.Collection;
 @NoArgsConstructor
 public class Course {
     @Id
+    @NotBlank(message = "Le sigle du cours est obligatoire")
     private String acronym;
+    @NotBlank(message = "Le titre du cours est obligatoire")
     private String title;
+    @Min(value = 1, message = "Le nombre de crédits doit être supérieur à 0")
+    @Max(value = 7, message = "Le nombre de crédits doit être inférieur à 8")
     private int credits;
     @ManyToMany(mappedBy = "courses")
     private Collection<Student> students;
--- src/main/java/g61453/web/pae/web/CourseCtrl.java
+++ src/main/java/g61453/web/pae/web/CourseCtrl.java
@@ -2,13 +2,16 @@ package g61453.web.pae.web;
 
 import g61453.web.pae.model.Course;
 import g61453.web.pae.service.PaeService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Optional;
 
@@ -21,7 +24,11 @@ public class CourseCtrl {
     public String index(Model model) {
         Iterable<Course> courses = paeService.getCourses();
         model.addAttribute("courses", courses);
-        model.addAttribute("course", new Course());
+        if (!model.containsAttribute("course")) {
+            // Les erreurs sont lié à un objet cours spécifique donc si il change Spring ne trouvera pas les erreurs
+            // puisque c'est un nouvel objet et donc ne seront pas affiché dans notre vue thymeleaf
+            model.addAttribute("course", new Course());
+        }
         return "courses";
     }
 
@@ -38,8 +45,14 @@ public class CourseCtrl {
     }
 
     @PostMapping("/courses/add-course")
-    public String addCourse(Course course) {
-        paeService.addCourse(course);
+    public String addCourse(@Valid Course course, BindingResult br, RedirectAttributes ra) {
+        if (!br.hasErrors()) {
+            paeService.addCourse(course);
+            ra.addFlashAttribute("success", course.getTitle() + " a été ajouté");
+        } else {
+            ra.addFlashAttribute("org.springframework.validation.BindingResult.course", br);
+            ra.addFlashAttribute("course", course);
+        }
         return "redirect:/courses";
     }
 }
